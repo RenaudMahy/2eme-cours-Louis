c9c1cf85596e038514dd16b7c2c6f859cf38e376
unknown <MSI-PC@MSI>
2023-10-13 19:07:14 +0100

    Ajoutez des fichiers projet.

---
+++ 2eme cours Louis/Personnage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2eme_cours_Louis
+{
+
+    // acces: public, type: classe, nom de la classe: personnage
+    public class Personnage
+    {
+        public string _nom; //par convention on nome une variable de cette facon: underscore+nom en minuscule
+
+        public int _age;
+
+        public string Nom //propriete (variable ++) s'appele Nom car elle est liée a la variable _nom
+        {
+            get
+            {
+                Console.WriteLine("get du nom est appelé");
+                return _nom;
+            }
+
+            set
+            {
+                Console.WriteLine("set du nom est appelé");
+                _nom = value;
+            }
+        }
+        public string Sexe { get; set; } // reviens au meme que ce qu'on a fait pour "Nom" mais le get et le set sont sous-entendu
+
+        public int PV { get; set; } 
+
+        public int PA { get; set; }
+
+        public Personnage(string nom, int age, string sexe)//ici on crée un constructeur, une structure qui comprend l'instanciation d'une classe et tout ce qu'elle comporte (ici nom, age, sexe), s'ecrit toujours: acces+nomdelaclasse+parenthese
+        {
+            Console.WriteLine("classe Personnage est instanciée");
+            Nom = nom;
+            _age = age;
+            Sexe = sexe;
+            PV = 100;
+            PA = 5;
+        }
+
+        public void Attaque(Personnage adversaire) // ici on crée une methode (une action): acces, void(car pas de retour), et on la nomme et dans parenthese le sujet de l'acttion
+        {
+            Console.WriteLine($"{Nom} attaque {adversaire.Nom}");
+            adversaire.PV -= PA;
+        }
+
+        public int GetPV()
+        {
+            return PV;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ 2eme cours Louis/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _2eme_cours_Louis;
+
+Console.WriteLine("Hello, World!");
+
+//ici, on instancie une classe, c'est a dire que je vais creer un personnage qui contiendra toute les infos déclarée dans la classe Personnage
+Personnage Renaud = new Personnage("Renaud", 29, "Mâle"); //nom de la classe appelée, nom donné a l'instanciation, puis TOUJOURS = new nomdelaclasseappelée
+
+//Renaud.Nom = "Renaud";
+//Renaud._age = 29;
+//Renaud.Sexe = "Mâle";
+
+Personnage Louis = new Personnage("Louis", 28, "Mâle");
+
+//Louis.Nom = "Louis";
+//Louis._age = 28;
+//Louis.Sexe = "Mâle";
+
+Personnage Anton = new Personnage("Anton", 28, "Femelle");
+
+// controle+k+c mettre tout en commentaire et controle+k+u remettre tout normale
+
+Console.WriteLine(Renaud.Nom);
+Console.WriteLine(Renaud._age);
+Console.WriteLine(Renaud.Sexe);
+
+Console.WriteLine(Louis.Nom);
+Console.WriteLine(Louis.PV);
+Renaud.Attaque(Louis);
+Console.WriteLine(Louis.Nom);
+Console.WriteLine(Louis.PV);
+
+
+Console.WriteLine(Anton.Nom);
+Console.WriteLine(Anton.PV);
+Renaud.Attaque(Anton);
+Console.WriteLine(Anton.Nom);
+Console.WriteLine(Anton.PV);
+
+Console.WriteLine(Renaud.PV);
+Console.WriteLine(Renaud.GetPV());
+
